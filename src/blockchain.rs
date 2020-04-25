2d13c54850c555260c4ab11531d13cebfc5f0112
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2020-04-25 12:39:34 +0200

    calculating block hashes

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,30 +1,49 @@
 use chrono::prelude::*;
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+use serde::Serialize;
+use serde_json;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct Transaction {
     pub sender: String,
     pub recipient: String,
     pub amount: f64,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct Block {
     pub index: i64,
     pub timestamp: i64,
     pub previous_hash: String,
+    pub hash: String,
     pub transactions: Vec<Transaction>,
 }
 
 impl Block {
     pub fn new(index: i64, previous_hash: String, transactions: Vec<Transaction>) -> Block { 
-        let block = Block {
+        let mut block = Block {
             index: index,
-            previous_hash: previous_hash,
             timestamp: Utc::now().timestamp_millis(),
+            previous_hash: previous_hash,
+            hash: String::new(),
             transactions: transactions,
         };
+        block.hash = block.calculate_hash();
 
         block
+    }
+
+    pub fn calculate_hash(&self) -> String {
+        // the "hash" field could be setted, so to be consistent we need to make sure we don't use it
+        let mut hashable_data = self.clone();
+        hashable_data.hash = String::new();
+
+        let serialized = serde_json::to_string(&hashable_data).unwrap();
+        let mut hasher = Sha256::new();
+        hasher.input_str(&serialized);
+
+        hasher.result_str()
     }
 }
 
@@ -49,7 +68,7 @@ impl Blockchain {
 
     pub fn add_block(&mut self, transactions: Vec<Transaction>) -> Block {
         let index = (self.current_block.index + 1) as i64;
-        let previous_hash = String::new();
+        let previous_hash = self.current_block.hash.clone();
         let block = Block::new(index, previous_hash, transactions);
 
         self.blocks.push(block.clone());

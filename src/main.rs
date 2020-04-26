eb65bdac1215706f501d195d5b126fac7d085254
mrnaveira <mrnaveira@gmail.com>
2020-04-26 20:59:11 +0200

    rest api now returns a list of blockchain blocks

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 mod api;
 mod blockchain;
 
-use blockchain::Blockchain;
+use blockchain::{Blockchain, Transaction};
 
 fn main() {
+    // genesis block
     let mut blockchain = Blockchain::new();
-    println!("{:?}", blockchain.current_block);
 
-    blockchain.add_block(Vec::new());
-    println!("{:?}", blockchain.current_block);
+    // block 1
+    let mut transactions_block_1 = Vec::new();
+    transactions_block_1.push(Transaction {
+        sender: String::from("1"),
+        recipient: String::from("2"),
+        amount: 1000
+    });
+    blockchain.add_block(transactions_block_1);
 
-    blockchain.add_block(Vec::new());
-    println!("{:?}", blockchain.current_block);
+    // block 2
+    let mut transactions_block_2 = Vec::new();
+    transactions_block_2.push(Transaction {
+        sender: String::from("2"),
+        recipient: String::from("1"),
+        amount: 500
+    });
+    transactions_block_2.push(Transaction {
+        sender: String::from("2"),
+        recipient: String::from("3"),
+        amount: 500
+    });
+    blockchain.add_block(transactions_block_2);
 
     let port = 8000;
-    api::run(port).expect("could not start the API");
+    api::run(port, blockchain).expect("could not start the API");
 }

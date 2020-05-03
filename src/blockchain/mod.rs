7a6ff5b875075e0d698d754fda00d55e9a67c0f9
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2020-05-03 21:45:40 +0200

    Refactor mod exports for simplification

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -1,3 +1,9 @@
-pub mod block;
-pub mod blockchain;
-pub mod transaction;
+mod block;
+mod blockchain;
+mod transaction;
+
+// Explicitly controlling which individual identifiers we export
+// It also avoids verbose module imports from other files
+pub use block::Block;
+pub use blockchain::Blockchain;
+pub use transaction::Transaction;

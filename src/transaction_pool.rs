20c777994fecf0aecb84b755b0258657717f279d
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2021-07-18 20:21:36 +0100

    Add tests for the transaction pool

--- src/transaction_pool.rs
+++ src/transaction_pool.rs
@@ -46,3 +46,64 @@ impl TransactionPool {
         return transactions_clone;
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_be_empty_after_creation() {
+        let transaction_pool = TransactionPool::new();
+
+        let transactions = transaction_pool.pop();
+        assert!(transactions.is_empty());
+    }
+
+    #[test]
+    fn should_pop_single_value() {
+        let transaction_pool = TransactionPool::new();
+
+        // add a new transaction to the pool
+        let transaction = create_mock_transaction(1);
+        transaction_pool.add_transaction(transaction.clone());
+
+        // pop the values and check that the transaction is included
+        let mut transactions = transaction_pool.pop();
+        assert_eq!(transactions.len(), 1);
+        assert_eq!(transactions[0].amount, transaction.amount);
+
+        // after the previous pop, the pool should still be empty
+        transactions = transaction_pool.pop();
+        assert!(transactions.is_empty());
+    }
+
+    #[test]
+    fn should_pop_multiple_values() {
+        let transaction_pool = TransactionPool::new();
+
+        // add a new transaction to the pool
+        let transaction_a = create_mock_transaction(1);
+        let transaction_b = create_mock_transaction(2);
+        transaction_pool.add_transaction(transaction_a.clone());
+        transaction_pool.add_transaction(transaction_b.clone());
+
+        // pop the values and check that the transactions are included
+        let mut transactions = transaction_pool.pop();
+        assert_eq!(transactions.len(), 2);
+        assert_eq!(transactions[0].amount, transaction_a.amount);
+        assert_eq!(transactions[1].amount, transaction_b.amount);
+
+        // after the previous pop, the pool should still be empty
+        transactions = transaction_pool.pop();
+        assert!(transactions.is_empty());
+    }
+
+    fn create_mock_transaction(amount: u64) -> Transaction {
+        Transaction {
+            sender: "1".to_string(),
+            recipient: "2".to_string(),
+            amount: amount
+        }
+    }
+}    

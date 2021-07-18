21fc4b81f86d2ba2914db97b5789d04621eef00d
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2021-07-18 12:33:42 +0100

    Add tests for blockchain initialization and refactor unnecessary clones

--- src/blockchain/blockchain.rs
+++ src/blockchain/blockchain.rs
@@ -4,36 +4,35 @@ use crate::blockchain::block::{Block, BlockHash};
 
 pub type BlockVec = Vec<Block>;
 
-// We don't need to export these types because concurrency is encapsulated in this file
-type SyncedBlock = Arc<Mutex<Block>>;
+// We don't need to export this because concurrency is encapsulated in this file
 type SyncedBlockVec = Arc<Mutex<BlockVec>>;
 
 #[derive(Debug, Clone)]
 pub struct Blockchain {
-    last_block: SyncedBlock,
     blocks: SyncedBlockVec,
 }
 
 impl Blockchain {
     pub fn new() -> Blockchain {
         let genesis_block = Blockchain::create_genesis_block();
-        let synced_genesis_block = Arc::new(Mutex::new(genesis_block.clone()));
 
+        // add the genesis block to the synced vec of blocks
         let mut blocks = BlockVec::default();
-        blocks.push(genesis_block.clone());
-        let synced_blocks =  Arc::new(Mutex::new(blocks.clone()));
+        blocks.push(genesis_block);
+        let synced_blocks =  Arc::new(Mutex::new(blocks));
 
         let blockchain = Blockchain {
-            blocks: synced_blocks.clone(),
-            last_block: synced_genesis_block.clone(),
+            blocks: synced_blocks,
         };
 
         return blockchain;
     }
 
     pub fn get_last_block(&self) -> Block {
-        let last_block = self.last_block.lock().unwrap();
-        return last_block.clone();
+        let blocks = self.blocks.lock().unwrap();
+        let last_block = blocks[blocks.len() - 1].clone();
+
+        return last_block;
     }
 
     pub fn get_all_blocks(&self) -> BlockVec {
@@ -43,10 +42,8 @@ impl Blockchain {
 
     pub fn add_block(&self, block: Block) {
         let mut blocks = self.blocks.lock().unwrap();
-        let mut last_block = self.last_block.lock().unwrap();
 
         blocks.push(block.clone());
-        *last_block = block.clone();
     }
 
     fn create_genesis_block() -> Block {
@@ -61,4 +58,28 @@ impl Blockchain {
 
 impl Default for Blockchain {
     fn default() -> Self { Blockchain::new() }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_have_valid_genesis_block() {
+        let blockchain = Blockchain::new();
+
+        // check that a new blockchain has one and only one block
+        let blocks = blockchain.get_all_blocks();
+        assert_eq!(blocks.len(), 1);    
+
+        // check that the last block is in the blockchain
+        let block = blockchain.get_last_block();
+        assert_eq!(block.hash, blocks[0].hash);
+
+        // check that the genesis block has valid values
+        assert_eq!(block.index, 0);
+        assert_eq!(block.nonce, 0);
+        assert_eq!(block.previous_hash, BlockHash::default());
+        assert!(block.transactions.is_empty());
+    }
 }

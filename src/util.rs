ee94c6798d2b5bb7508baf2ad2415a084e240886
miguel <mrnaveira@gmail.com>
2021-10-16 18:46:47 +0100

    Refactor the 'util' module to explicitly export the 'termination' submodule

--- src/util.rs
+++ src/util.rs
@@ -2,11 +2,10 @@ mod config;
 mod context;
 pub mod execution;
 mod logger;
-mod termination;
+pub mod termination;
 
 // Explicitly controlling which individual identifiers we export
 // It also avoids verbose module imports from other files
 pub use config::Config;
 pub use context::Context;
 pub use logger::initialize_logger;
-pub use termination::set_ctrlc_handler;

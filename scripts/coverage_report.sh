869fc850c708b02960f44c9c601c75cb7b595dcb
Miguel Naveira <47919901+mrnaveira@users.noreply.github.com>
2022-06-26 17:28:13 +0100

    Update coverage calculation (#7)
    * update the local coverage script
    
    * update the coverage github action

--- scripts/coverage_report.sh
+++ scripts/coverage_report.sh
@@ -1,7 +1,5 @@
 # This script generates a test coverage report under the "/coverage" folder
-# To run it, at the moment it's required to use the nightly Rust version:
-#       $ rustup install nightly
-#       $ rustup default nightly
+# To run it you will need Rust version 1.60 or greater and install the following:
 #       $ cargo install grcov
 #       $ rustup component add llvm-tools-preview
 
@@ -12,9 +10,10 @@ cargo clean
 rm -rf ./coverage ./target *.prof*
 
 # Export the flags needed to instrument the program to collect code coverage.
-export RUSTFLAGS="-Zinstrument-coverage"
+export RUSTFLAGS="-C instrument-coverage"
 
-# Ensure each test runs gets its own profile information by defining the LLVM_PROFILE_FILE environment variable (%p will be replaced by the process ID, and %m by the binary signature):
+# Ensure each test runs gets its own profile information by defining the LLVM_PROFILE_FILE environment variable
+# (%p will be replaced by the process ID, and %m by the binary signature):
 export LLVM_PROFILE_FILE="rust_blockchain-%p-%m.profraw"
 
 # Build the program
